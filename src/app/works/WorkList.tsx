a6404c5d42a8908a56e29a99ec7c5cd810412f1f
june9999 <vesper0@163.com>
2024-01-21 13:08:32 +0100

    “fix”

--- src/app/works/WorkList.tsx
+++ src/app/works/WorkList.tsx
@@ -1,17 +1,15 @@
 "use client";
 import React from "react";
-import useFetch from "@/lib/useFetch";
 
 const WorkList = () => {
   // in this case, I want to get the data from some api and update the status
-
-  const customerFetch = async (args: string) => {
-    const res = await fetch(args);
-    return await res.json();
-  };
-  const [data, loading, error] = useFetch("apo/work", customerFetch);
-  console.log(data);
-  return <div>WorkList</div>;
+  // const customerFetch = async (args: string) => {
+  //   const res = await fetch(args);
+  //   return await res.json();
+  // };
+  // const [data, loading, error] = useFetch("apo/work", customerFetch);
+  // console.log(data);
+  // return <div>WorkList</div>;
 };
 
 export default WorkList;
